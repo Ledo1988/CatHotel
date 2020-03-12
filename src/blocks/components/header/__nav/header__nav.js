7cb69f1bfaf39a10a99f4bcd1ff5dc92c12f6079
Elena <elena.do1988@yandex.ru>
2020-03-12 14:17:15 +0300

    fonts, images, sass variables are added

---
+++ src/blocks/components/header/__nav/header__nav.js
@@ -0,0 +1,13 @@
+modules.define('header__nav', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declElem('header', 'nav', {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
