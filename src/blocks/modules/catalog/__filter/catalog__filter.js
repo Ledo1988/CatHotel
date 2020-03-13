f3ee512432c5515ed378b03ca4a77e7d8f0686cb
Elena <elena.do1988@yandex.ru>
2020-03-13 13:26:42 +0300

    filter reset option is added

--- src/blocks/modules/catalog/__filter/catalog__filter.js
+++ src/blocks/modules/catalog/__filter/catalog__filter.js
@@ -14,4 +14,31 @@ $(document).ready(function() {
         $('html').toggleClass('overflow-hidden');
     });
 
+    $('body .catalog__filter-form').on('change input', 'input', function() {
+        catalogFormValidation($('body .catalog__filter-form'));
+    });
+
+    $('body .catalog__filter-form').on('click', '.catalog__filter-btn[type="reset"]', function() {
+        $('body .catalog__filter-form').toggleClass('not-empty');
+    });
+
+    function catalogFormValidation($this) {
+        var isEmpty = true;
+
+        $this.find("input").each(function() {
+            var element = $(this);
+
+            if (!element.val().length == 0 || element.prop("checked") == true ) {
+                isEmpty = false;
+            }
+        });
+
+        if (!isEmpty) {
+            $('body .catalog__filter-form').addClass('not-empty');
+        } else {
+            $('body .catalog__filter-form').removeClass('not-empty');
+        }
+
+
+    }
 });
